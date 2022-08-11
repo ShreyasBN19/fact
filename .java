55736d07945bc71b6ae9c7bd69981a65ebf67254
shamabhatt <111080651+shamabhatt@users.noreply.github.com>
2022-08-12 01:15:31 +0530

    Update and rename .gitignore to .java

---
+++ .java
@@ -0,0 +1,13 @@
+class fact
+{  
+ public static void main(String[] args)
+ {  
+   int i,f=1;  
+   int num=5;
+      for(i=1;i<=num;i++)
+      {    
+        f=f*i;    
+      }    
+   System.out.println("factorial of "+num+" is:"+f);    
+ }  
+}  
