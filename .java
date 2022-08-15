7b4b36012a4691e79b493168dce539a95dacc7a8
shama <shamabhat1111@gmail.com>
2022-08-15 23:44:57 +0530

    second commit

--- .java
+++ .java
@@ -3,11 +3,11 @@ class fact
  public static void main(String[] args)
  {  
    int i,f=1;  
-   int num=5;
-      for(i=1;i<=num;i++)
+   int n=5;
+      for(i=1;i<=n;i++)
       {    
         f=f*i;    
       }    
-   System.out.println("factorial of "+num+" is:"+f);    
+   System.out.println("factorial of "+n+" is:"+f);    
  }  
 }  
